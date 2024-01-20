a73b457cb9e432bdfc88f64f4a748c839b726e94
Ilya Dubatovka <ilya.dubatovka@mail.ru>
2024-01-20 21:52:49 +0300

    feat: add sum of numbers

--- github_test/Program.cs
+++ github_test/Program.cs
@@ -6,7 +6,12 @@ namespace github_test
     {
         static void Main(string[] args)
         {
- 
+            Console.WriteLine("Введите первое число:");
+            double firstNumber = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите второе число:");
+            double secondNumber = Convert.ToDouble(Console.ReadLine());
+            double sum = firstNumber + secondNumber;
+            Console.WriteLine("Результат сложения " + sum);
         }
     }
 }
